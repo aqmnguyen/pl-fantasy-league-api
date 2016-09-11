f1d940d4ce09c0e3cb6028b080a02f0892a4c152
lep828 <lep828@gmail.com>
2016-09-11 17:05:38 +0100

    removing global variablse from players and teams

--- routes/players.js
+++ routes/players.js
@@ -3,17 +3,17 @@ var router = express.Router();
 var PL = require('../shared');
 var shared = new PL();
 
-var content = {
-	title : 'Premier League Fantasy League API - Players',
-	header : 'Players'
-}
-
 /* GET home page. */
 router.get('/', function(req, res, next) {
 	shared.getHTMLContent('https://www.fantasyleague.com/Classic/Stats/playerlist.aspx?dpt=0')
 	.then(function(response){
-		//console.log(response);
+
+		var content = {
+			title : 'Premier League Fantasy League API - Players',
+			header : 'Players'
+		}
 		var players = shared.lastPlayer(response);
+
 		content.playerList = players;
 		res.setHeader('Content-Type', 'application/json');
 		res.render('players', content);
@@ -25,10 +25,14 @@ router.get('/:id', function(req, res, next) {
 
 	shared.getHTMLContent('https://www.fantasyleague.com/Classic/Stats/playerlist.aspx?dpt=0')
 	.then(function(response){
-		//console.log(response);
+		
+		var content = {
+			title : 'Premier League Fantasy League API - Players',
+			header : 'Players'
+		}
 		var players = shared.lastPlayer(response);
+		
 		content.playerList = players;
-		//search for that player based on the id
 		content.playerList = shared.playerSearchName(content.playerList, req.params.id);
 		content.search = req.params.id;
 		res.setHeader('Content-Type', 'application/json');
